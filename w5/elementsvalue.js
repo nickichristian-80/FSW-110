4763711c9d2c0afdd016a91cd9e130c33d588fa3
nickichristian-80 <nicki.christian@bryanuniversity.edu>
2020-07-17 15:05:15 -0500

    commit

---
+++ w5/elementsvalue.js
@@ -0,0 +1,12 @@
+
+
+
+function display()
+{
+var x=document.details.fname.value;
+var y=document.details.lname.value;
+var z=document.details.phnum.value;
+alert("Hello, "+ x+" "+y+"     "+"Phone Number:"+z);
+
+document.getElementById("myForm").reset();
+}
